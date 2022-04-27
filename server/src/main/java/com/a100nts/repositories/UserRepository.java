0e85097da1d048b8c89fe847e74620784ed720b6
tihomirbg4 <tihomirbg4@gmail.com>
2022-04-27 21:00:56 +0300

    Added register and login functionality

--- server/src/main/java/com/a100nts/repositories/UserRepository.java
+++ server/src/main/java/com/a100nts/repositories/UserRepository.java
@@ -2,6 +2,7 @@ package com.a100nts.repositories;
 
 import com.a100nts.models.User;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 
 import java.util.Optional;
 
@@ -9,4 +10,5 @@ public interface UserRepository extends JpaRepository<User, Long> {
 
     User findUserById(Long id);
     Optional<User> findByEmail(String email);
+
 }
--- server/src/main/java/com/a100nts/services/UserServiceImpl.java
+++ server/src/main/java/com/a100nts/services/UserServiceImpl.java
@@ -4,9 +4,11 @@ import com.a100nts.models.User;
 import com.a100nts.repositories.UserRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
-import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
+
+import java.nio.charset.StandardCharsets;
+import java.util.Base64;
 
 @Service
 public class UserServiceImpl implements UserService {
@@ -14,15 +16,12 @@ public class UserServiceImpl implements UserService {
     @Autowired
     private UserRepository userRepository;
 
-    @Bean
-    public PasswordEncoder passwordEncoder() {
-        return new BCryptPasswordEncoder();
-    }
+    @Autowired
+    private PasswordEncoder passwordEncoder;
 
     @Override
     public User registerUser(User user) {
-        user.setPassword(passwordEncoder().encode(user.getPassword()));
-
+        user.setPassword(passwordEncoder.encode(user.getPassword()));
         return userRepository.save(user);
     }
 
