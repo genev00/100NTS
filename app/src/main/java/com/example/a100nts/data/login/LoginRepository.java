6b5a2da77b80e5abd87fd408f02097776258c3f9
Yordan Genev <57504749+genev00@users.noreply.github.com>
2022-05-01 14:56:34 +0300

    add own omage hosting

--- app/src/main/java/com/example/a100nts/data/login/LoginRepository.java
+++ app/src/main/java/com/example/a100nts/data/login/LoginRepository.java
@@ -1,21 +1,38 @@
 package com.example.a100nts.data.login;
 
-import com.example.a100nts.entities.UserUI;
+import static com.example.a100nts.common.RestUtil.OBJECT_MAPPER;
+import static com.example.a100nts.common.RestUtil.REST_TEMPLATE;
+import static com.example.a100nts.common.RestUtil.SERVER_URL;
+import static com.example.a100nts.common.StringProvider.getTranslatedString;
+import static com.example.a100nts.ui.login.LoginActivity.loginActivity;
+import static com.example.a100nts.ui.register.RegisterActivity.setRegistrationData;
 
+import android.content.Intent;
+
+import com.example.a100nts.R;
+import com.example.a100nts.entities.User;
+import com.example.a100nts.entities.UserUI;
+import com.example.a100nts.ui.register.RegisterActivity;
+
+import org.springframework.http.HttpEntity;
+import org.springframework.http.HttpMethod;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+
+import java.io.IOException;
+
+import lombok.AccessLevel;
+import lombok.NoArgsConstructor;
+
+@NoArgsConstructor(access = AccessLevel.PRIVATE)
 public class LoginRepository {
 
     private static volatile LoginRepository instance;
     private static UserUI loggedUser = null;
 
-    private final LoginDataSource loginDataSource;
-
-    private LoginRepository(LoginDataSource loginDataSource) {
-        this.loginDataSource = loginDataSource;
-    }
-
-    public static LoginRepository getInstance(LoginDataSource loginDataSource) {
+    public static LoginRepository getInstance() {
         if (instance == null) {
-            instance = new LoginRepository(loginDataSource);
+            instance = new LoginRepository();
         }
         return instance;
     }
@@ -25,7 +42,7 @@ public class LoginRepository {
     }
 
     public Result login(String email, String password) {
-        Result result = loginDataSource.login(email, password);
+        Result result = restLogin(email, password);
         if (result instanceof Result.Success) {
             loggedUser = ((Result.Success<UserUI>) result).getData();
         }
@@ -38,6 +55,37 @@ public class LoginRepository {
 
     public static synchronized void setLoggedUser(UserUI loggedUser) {
         LoginRepository.loggedUser = loggedUser;
+    }
+
+    public static Result restLogin(String email, String password) {
+        try {
+            ResponseEntity<? extends Boolean> checkEmailResult = REST_TEMPLATE.getForEntity(
+                    SERVER_URL + "/emailExists" + '/' + email, Boolean.class
+            );
+            if (checkEmailResult.getStatusCode() == HttpStatus.OK && !checkEmailResult.getBody()) {
+                Intent register = new Intent(loginActivity, RegisterActivity.class);
+                setRegistrationData(email, password);
+                loginActivity.startActivity(register);
+                return new Result.Success<>(getLoggedUser());
+            }
+
+            HttpEntity<User> user = new HttpEntity<>(new User(email, password));
+            ResponseEntity<?> result = REST_TEMPLATE.exchange(
+                    SERVER_URL + "/login", HttpMethod.POST, user, Object.class
+            );
+
+            if (result.getStatusCode() == HttpStatus.OK) {
+                return new Result.Success<>(OBJECT_MAPPER.convertValue(result.getBody(), UserUI.class));
+            } else {
+                return new Result.Error(new IOException((String) result.getBody()));
+            }
+        } catch (Exception e) {
+            if (e.getMessage() != null &&
+                    e.getMessage().contains(HttpStatus.FORBIDDEN.toString())) {
+                return new Result.Error(new IOException(getTranslatedString(R.string.wrong_password)));
+            }
+            return new Result.Error(new IOException(getTranslatedString(R.string.login_failed) + ' ' + e.getMessage(), e));
+        }
     }
 
 }
