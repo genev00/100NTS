5cdf6d80cfa23027a0b3e004084bf80b0562579f
Yordan Genev <57504749+genev00@users.noreply.github.com>
2022-05-01 14:21:45 +0300

    fix Site<->Image mapping

--- server/src/main/java/com/a100nts/services/impl/DbSeedService.java
+++ server/src/main/java/com/a100nts/services/impl/DbSeedService.java
@@ -27,6 +27,7 @@ public class DbSeedService {
             File file = new File(DbSeedService.class.getClassLoader().getResource(SITES_DATA_FILE).toURI());
             ObjectMapper objectMapper = new ObjectMapper();
             parsedSites = objectMapper.readValue(file, Site[].class);
+            Arrays.stream(parsedSites).forEach(s -> s.getImages().forEach(i -> i.setSite(s)));
         } catch (Exception e) {
             e.printStackTrace();
             System.exit(1);
